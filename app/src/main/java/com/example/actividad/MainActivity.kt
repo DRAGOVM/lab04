134fea0633030bb6f18bf11dddb690f2b0150ebd
Victor <Victor@example.com>
2025-09-09 12:40:37 -0500

    Segundo Commit

--- app/src/main/java/com/example/actividad/MainActivity.kt
+++ app/src/main/java/com/example/actividad/MainActivity.kt
@@ -38,6 +38,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun Metodo1(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Modelo1 $name!",
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
